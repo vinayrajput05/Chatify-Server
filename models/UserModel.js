a832e4525090e1107bf72d82fc26988b92f5dcc5
codewithvinaysingh <codewithvinaysingh@gmail.com>
2024-08-07 09:04:02 +0000

    Auth Model Done

---
+++ models/UserModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { getSalt } from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+    color: {
+        type: Number,
+        required: false,
+    },
+    profileSetup: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    // if (!this.isModified('password')) {
+    //     next();
+    // }
+
+    const salt = await getSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User= mongoose.model('User', userSchema);
+
+export default User;
