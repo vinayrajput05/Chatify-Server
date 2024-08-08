08ca4a4026b5cce3dbf32f546efe5fbbb47e1005
codewithvinaysingh <codewithvinaysingh@gmail.com>
2024-08-08 07:52:37 +0000

    update cors

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ const app = express();
 const port = process.env.PORT || 3000
 const databaseUrl = process.env.DATABASE_URL
 
+// for development only 
 app.use(cors({
-  origin: ["*"],
+  origin: (origin, callback) => {
+    if (!origin || origin.startsWith('http://localhost') || origin.startsWith('https://your-allowed-domain.com')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 }));
+
+// for production
+// app.use(cors({
+//   origin: [process.env.ORIGIN],
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+//   credentials: true,
+// }));
 
 app.use(cookieParser());
 app.use(express.json());
